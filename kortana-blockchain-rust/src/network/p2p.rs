278524a72baf5338c51aa1b98b9695ffdebde0c5
Emeka Iwuagwu <iwuagwuemmy@yahoo.com>
2026-02-15 08:25:01 +0100

    fix: resolve syntax error (extra brace) in p2p.rs blocking build

--- kortana-blockchain-rust/src/network/p2p.rs
+++ kortana-blockchain-rust/src/network/p2p.rs
@@ -122,7 +122,6 @@ impl KortanaNetwork {
                                 }
                             }
                         }
-                        }
                         _ => {}
                     }
                     _ => {}
